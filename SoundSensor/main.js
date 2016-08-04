4cd06cdacc0dd55e1475cf4cb166e13539fe2251
Vivek Sharanappa <vivek.hs@gmail.com>
2016-08-04 19:21:07 +0530

    updates sources
    synced all my sources

---
+++ SoundSensor/main.js
@@ -0,0 +1,33 @@
+var upmMicrophone = require("jsupm_mic");
+// Attach microphone to analog port A0
+var myMic = new upmMicrophone.Microphone(2);
+
+var threshContext = new upmMicrophone.thresholdContext;
+threshContext.averageReading = 0;
+threshContext.runningAverage = 0;
+threshContext.averagedOver = 2;
+
+var is_running = false;
+// Infinite loop, ends when script is cancelled
+// Repeatedly, take a sample every 2 microseconds;
+// find the average of 128 samples; and
+// print a running graph of the averages
+while(1)
+{
+    var buffer = new upmMicrophone.uint16Array(128);
+    var len = myMic.getSampledWindow(2, 128, buffer);
+    if (len)
+    {
+        var thresh = myMic.findThreshold(threshContext, 30, buffer, len);
+        myMic.printGraph(threshContext);
+        if (thresh)
+            console.log("Threshold is " + thresh);
+    }
+}
+
+// Print message when exiting
+process.on('SIGINT', function()
+{
+	console.log("Exiting...");
+	process.exit(0);
+});
